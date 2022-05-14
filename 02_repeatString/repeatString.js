38c27528df73513c5200854e32b89ace4ee9495c
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-05-14 22:56:56 +0000

    added a loop and used the concat method for the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    for (i=0; i<n; i++){
+      //concat joins something together
+      string2 = string.concat(string)
+      string3 = string.concat(string2) 
+    }
+     return string3;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
