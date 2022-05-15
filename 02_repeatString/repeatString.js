9c13267e253f630072939ef5c7ce15c85f65cbd8
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-05-15 00:52:25 +0000

    exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,23 @@
 const repeatString = function(string, n) {
+  //for every number =0 or greater, return a repetition of that number
+  //use for loop to repeat the string
     for (i=0; i<=n; i++){
-      //concat joins something together
-      //string 2 = "heyhey"
+
+      //repeat string and return it
       string = string.repeat(n);
       return string;
+
+      //OLD FUNCTION
+       //concat joins something together
+      //string 2 = "heyhey"
      // string2 = string.concat(string)
     //  string3 = string.concat(string2) 
     } 
+
+  //returns ERROR if n is a negative number
+  for (i=0; i>n; i--){
+    return "ERROR";
+  }
 
 };
 
