7437a90921d5fa803a25c6ebd3e8bb13a0a6d94d
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-05-25 23:24:52 +0000

    converted a string to array to reverse the order

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  //maybe convert a string to an array??
+  // use '' to reverse the characters 
+  let array = string.split('');
 
+  //reverse the array
+  array2 = array.reverse();
+
+  //turn array to string
+  // remove the commas from the array
+  let finalstring = array2.join('');
+
+  return finalstring;
+  
 };
 
 // Do not edit below this line
