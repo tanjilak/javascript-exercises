1d25355d566a4346e6ff2fcd077ceea6ffdc7780
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-06 20:15:15 +0000

    first test case passed by looping splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function(arr,int) {
-arr.splice(int);
+  //use indexOf to search for the element
+let index = arr.indexOf(int);
+  //loop if element is searched 
+  //remove the searched element after the first position
+  if (index > -1){
+  arr.splice(index, 1);
+  }
+
+  //return the new a
+  return arr;
 };
 
 // Do not edit below this line
