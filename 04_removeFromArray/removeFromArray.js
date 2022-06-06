7b8af0380369408f3f47b827bbbd0688fc34ecb5
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-06 20:19:13 +0000

    exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(arr,int) {
+const removeFromArray = function(arr,int, int2, int3, int4) {
   //use indexOf to search for the element
 let index = arr.indexOf(int);
   //loop if element is searched 
@@ -7,7 +7,22 @@ let index = arr.indexOf(int);
   arr.splice(index, 1);
   }
 
-  //return the new a
+let index2 = arr.indexOf(int2);
+   if (index2 > -1){
+  arr.splice(index2, 1);
+  }
+
+  let index3 = arr.indexOf(int3);
+   if (index3 > -1){
+  arr.splice(index3, 1);
+  }
+
+  let index4 = arr.indexOf(int4);
+   if (index4 > -1){
+  arr.splice(index4, 1);
+  }
+  
+  //return the new array
   return arr;
 };
 
