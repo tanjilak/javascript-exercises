73f8716375893fef7b7fc6836a5c035b8748c6ea
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-08 03:41:35 +0000

    exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+
+//first if (divisible by 400) // works for 1600
+if (year % 400 == 0){
+return true;
+}
+
+// second if (divisible by 100) // works for 1900 
+
+else if (year % 100 == 0){
+  return false;
+}
+
+  //third if (divisible by 4)
+else if (year % 4 == 0){
+  return true;
+}
+
+
+else{
+  return false;
+}
 };
 
 // Do not edit below this line
