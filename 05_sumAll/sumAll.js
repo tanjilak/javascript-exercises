2a00f96f91f41f5dde8616398c06f16b75240388
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-08 02:46:07 +0000

    first test case completed // used mathematical formula

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+  n = int2;
+  for(let i=0; i<int2; i++){
+    sum = n/2*(int1 + int2)
+  }
+  return sum;
 };
 
 // Do not edit below this line
