e4824536d8f8fc8ecda898bed4587b3e2651d65a
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-08 03:59:24 +0000

    exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  //formula for f to c conversion
+  celsius = (f-32)*(5/9);
 
+  //round the answer to one decimal
+  // use Math.round()
+  //*100 for 2 decimal places // *10 for 1 decimal place
+  
+  celsiusrounded = Math.round((celsius + Number.EPSILON)*10) /10;
+
+  //return answer
+  return celsiusrounded;
 };
 
-const ctof = function() {
 
+//same concept
+const ctof = function(c) {
+  fh = 32+(c*(9/5));
+  fhrounded = Math.round((fh + Number.EPSILON)*10) /10
+  return fhrounded;
 };
 
 // Do not edit below this line
