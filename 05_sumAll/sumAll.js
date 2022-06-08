a4510a8853b9c029f9140ac64eb2e22562141c63
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-08 03:27:45 +0000

    exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,23 +8,42 @@ const sumAll = function(int1, int2) {
   //loop to find the sum
   
   for(let i=0; i<int2; i++){
-    //if in reverse order
-    if (int1 > int2){
-      sum = m/2*(int1+int2)
-    }
+
+    //if inputs are both numbers
+    //RUN THE CORRECT SUM 
+    
+    if (typeof int1==='number' && typeof int2 === 'number'){
+      
+      //if in reverse order
+      if (int1 > int2){
+        sum = m/2*(int1+int2)
+      }
+        
       //if negative number 
-    else if(int1 < 0 || int2 < 0){
-      return "ERROR"    
-    }
+      else if(int1 < 0 || int2 < 0){
+        return "ERROR"    
+      }
+
       //if not in reverse
-    else{
-    sum = n/2*(int1 + int2)
+      else{
+      sum = n/2*(int1 + int2)
+      }
+      
+       //return value
+      return sum;
+  }
+
+  //if its not a number (string)
+  else{
+      return "ERROR"
     }
+  
     
   }
 
-  //return value
-  return sum;
+  //add an error message if anything is undefined
+  return "ERROR"
+
 };
 
 // Do not edit below this line
